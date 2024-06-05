ad5f358678867e2c4ed5774ee96b6e4e452e7416
highhhopes <zeng9960@gmail.com>
2024-06-05 16:45:47 +0900

    java0520 반복문과 배열

---
+++ java0520/Study03.java
@@ -0,0 +1,24 @@
+package java0520;
+
+public class Study03 {
+	
+	public static void main(String[]args) {
+		int s = 0;
+		int a = 4;
+		
+/*		while(true) {
+
+			if(s == a) break;
+			System.out.println("반복문");
+			s++;
+		}
+	
+	*/
+	
+	
+		do {
+			System.out.println("반복문");
+		}while(a < s); 
+		
+	}
+}
